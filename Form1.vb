d88f6b76e561236709aae16fb89f89b95acd7416
Academy-User <Academy-User@DESKTOP-1L3B0TP>
2024-01-17 14:21:01 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim n As Integer
+
+        While n <> 5
+            n += 1
+
+            MsgBox(n)
+
+        End While
+
+        MsgBox("now out of the loop ")
+    End Sub
+End Class
